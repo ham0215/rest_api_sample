4fc5529a274f99c63f134964023d451be1647de1
hamadanaoto <teiru_3@yahoo.co.jp>
2020-01-18 22:39:12 +0900

    Add test case for users#show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
+RSpec.describe UsersController, type: :request do
+  describe "GET /users/:id" do
+    let(:user) { FactoryBot.create :user }
+    let(:params) do
+      {
+        id: user.id
+      }
+    end
+    let(:headers) { { 'Accept': 'application/json' } }
+
+    it "works!" do
+      get user_path(params), { headers: headers }
       expect(response).to have_http_status(200)
     end
   end
